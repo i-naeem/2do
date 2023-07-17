2343a410fffc1f2ad42f7d12314ea910a373d32d
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-17 11:00:18 +0500

    feat: Add `db.js`

---
+++ db.js
@@ -0,0 +1,17 @@
+const todoDatabase = [];
+const categoryDatabase = [];
+
+const createTodo = () => {};
+const updateTodo = (id, todo) => {};
+const removeTodo = () => {};
+const addTodo = todo => {};
+
+const createCategory = () => {};
+const removeCategory = () => {};
+const addCategory = category => {};
+const updateCategory = (id, category) => {};
+
+const getAllTodo = () => {};
+const getAllCategories = () => {};
+const getCategoryById = () => {};
+const getTodoById = () => {};
