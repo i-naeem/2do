d2f2ae0b91b176441bc40cab73c7853ec94b31f0
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-17 11:54:37 +0500

    render `todos` and `categories`

--- db.js
+++ db.js
@@ -1,5 +1,122 @@
-const todoDatabase = [];
-const categoryDatabase = [];
+const categoryDatabase = [
+  {
+    id: 1,
+    name: 'Twimm',
+  },
+  {
+    id: 2,
+    name: 'Dabtype',
+  },
+  {
+    id: 3,
+    name: 'Bubbletube',
+  },
+  {
+    id: 4,
+    name: 'Mita',
+  },
+  {
+    id: 5,
+    name: 'Voonte',
+  },
+  {
+    id: 6,
+    name: 'Trunyx',
+  },
+  {
+    id: 7,
+    name: 'Wikido',
+  },
+  {
+    id: 8,
+    name: 'Thoughtmix',
+  },
+  {
+    id: 9,
+    name: 'Brightbean',
+  },
+  {
+    id: 10,
+    name: 'Buzzbean',
+  },
+];
+const todoDatabase = [
+  {
+    id: 1,
+    text: 'Phasellus sit amet erat.',
+    categoryIds: [7, 9, 5],
+  },
+  {
+    id: 2,
+    text: 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.',
+    categoryIds: [],
+  },
+  {
+    id: 3,
+    text: 'Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.',
+    categoryIds: [10, 6],
+  },
+  {
+    id: 4,
+    text: 'Suspendisse potenti.',
+    categoryIds: [8, 10],
+  },
+  {
+    id: 5,
+    text: 'Quisque ut erat.',
+    categoryIds: [2, 1, 9],
+  },
+  {
+    id: 6,
+    text: 'Aliquam erat volutpat.',
+    categoryIds: [3],
+  },
+  {
+    id: 7,
+    text: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.',
+    categoryIds: [6, 8, 9],
+  },
+  {
+    id: 8,
+    text: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula.',
+    categoryIds: [10, 9],
+  },
+  {
+    id: 9,
+    text: 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.',
+    categoryIds: [],
+  },
+  {
+    id: 10,
+    text: 'Donec posuere metus vitae ipsum. Aliquam non mauris.',
+    categoryIds: [3],
+  },
+  {
+    id: 11,
+    text: 'Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
+    categoryIds: [5],
+  },
+  {
+    id: 12,
+    text: 'Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti.',
+    categoryIds: [2, 8, 1],
+  },
+  {
+    id: 13,
+    text: 'Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.',
+    categoryIds: [2],
+  },
+  {
+    id: 14,
+    text: 'Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.',
+    categoryIds: [1, 4],
+  },
+  {
+    id: 15,
+    text: 'In quis justo. Maecenas rhoncus aliquam lacus.',
+    categoryIds: [2],
+  },
+];
 
 const createTodo = todo => todoDatabase.push(todo);
 
--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+import db from './db.js';
+
 const categoryList = document.getElementById('ctl');
 const taskForm = document.getElementById('task-form');
+const allTodoList = document.getElementById('all-todo-list');
 const categoryForm = document.getElementById('category-form');
+const categorySelector = document.getElementById('category-selector');
+
+const allTodo = db.getAllTodo();
+const allCategories = db.getAllCategories();
+
+function renderCategories(categories) {
+  categories.forEach(ct => {
+    let option = document.createElement('option');
+    option.value = ct.id;
+    option.innerText = ct.name;
+    categorySelector.appendChild(option);
+  });
+}
+
+function renderTodo(todoArray) {
+  todoArray.forEach(todo => {
+    const cts = todo.categoryIds.map(cid => {
+      const ct = db.getCategoryById(cid);
+      return `<span>${ct}</span>`;
+    });
+
+    const liItem = document.createElement('li');
+    liItem.id = todo.id;
+    const t = `
+      <p>${todo.text}</p>
+      <div class="group">${cts.join(' ')}</div>
+      <div class="group">
+        <button id="done-btn">Done</button>
+        <button id="edit-btn">Edit</button>
+        <button id="remove-btn">Remove</button>
+      </div>
+    `;
+
+    liItem.innerHTML = t;
+    allTodoList.appendChild(liItem);
+  });
+}
+
+renderCategories(allCategories);
+renderTodo(allTodo);
