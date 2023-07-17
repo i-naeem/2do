547279f44d3ec2f3d8398ae2ab53ec3f4cd42cb3
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-17 11:22:46 +0500

    feat: define `updateCategory`

--- db.js
+++ db.js
@@ -3,21 +3,25 @@ const categoryDatabase = [];
 
 const createTodo = todo => todoDatabase.push(todo);
 
+const removeTodo = id => {
+  todoDatabase = todoDatabase.filter(t => t.id !== id);
+};
+
 const updateTodo = todo => {
   const todoIndex = todoDatabase.findIndex(t => t.id === todo.id);
   todoDatabase[todoIndex] = todo;
 };
 
-const removeTodo = id => {
-  todoDatabase = todoDatabase.filter(t => t.id !== id);
+const createCategory = ct => categoryDatabase.push(ct);
+
+const removeCategory = id => {
+  todoDatabase = todoDatabase.filter(c => c.id == id);
 };
 
-const addTodo = todo => {};
-
-const createCategory = () => {};
-const removeCategory = () => {};
-const addCategory = category => {};
-const updateCategory = (id, category) => {};
+const updateCategory = (id, ct) => {
+  const ctIndex = categoryDatabase.findIndex(c => c.id === id);
+  categoryDatabase[ctIndex] = ct;
+};
 
 const getAllTodo = () => {};
 const getAllCategories = () => {};
